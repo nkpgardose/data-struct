cc81535a9fdd545de1a8ca69220e883166160355
Neil Gardose <nkpgardose@gmail.com>
2017-06-18 01:37:12 +0800

    Add bubble sort
    * Add comparator function to modify condition of sort
    * Add bubble sort spec

--- bubble-sort/bubble-sort.js
+++ bubble-sort/bubble-sort.js
@@ -1,3 +1,23 @@
 export const bubblesort = (list, cmp) => {
-   return "hello"
+  const compare = cmp || ((a, b) => { return a - b })
+  const maxlen = list.length
+  let count = 0;
+  let isSwap = true;
+  let temp;
+
+  while((count < maxlen - 1) && isSwap) {
+    isSwap = false
+    count++
+
+    for(var i = 0; i < maxlen - count; i++) {
+      if (compare(list[i], list[i + 1]) > 0) {
+        temp = list[i]
+        list[i] = list[i + 1]
+        list[i + 1] = temp
+        isSwap = true
+      }
+    }
+  }
+
+  return list
 }
